dad893f027d27aa0bab0b0a036abd26cd3729c5e
Prateek Gupta <prateekjpg@gmail.com>
2024-03-06 17:27:22 +0530

    minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
 
-export default Hero
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/prateekguptaiiitk")}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+    </header>
+  );
+};
+
+export default Hero;
